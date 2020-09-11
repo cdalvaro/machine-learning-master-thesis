21fb5a7094c2ae6379a8c43a4d6d1e04cf434115
Carlos Álvaro <carlos@cdalvaro.io>
2020-09-11 23:30:01 +0200

    feat: Improve BaseCatalogue class

--- code/cdalvaro/catalogues/base_catalogue.py
+++ code/cdalvaro/catalogues/base_catalogue.py
@@ -1,17 +1,17 @@
 from abc import abstractmethod
 from typing import Dict, TypeVar
 
-from ..info import Cluster
+from ..models import Region
 
-Catalogue = TypeVar('Catalogue', bound=Dict[str, Cluster])
+Catalogue = TypeVar('Catalogue', bound=Dict[str, Region])
 
 
 class BaseCatalogue:
-
     @staticmethod
     @abstractmethod
     def load_catalogue() -> Catalogue:
         """
         Static method for loading the selected catalogue
         """
+        # TODO: Add parameter to fill incomplete fields
         raise NotImplementedError
