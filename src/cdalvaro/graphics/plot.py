2002d32116b2368063f45f8e0cc9953520ebfc86
Carlos Álvaro <carlos@cdalvaro.io>
2020-11-09 21:22:14 +0100

    feat: Update plots

--- src/cdalvaro/graphics/plot.py
+++ src/cdalvaro/graphics/plot.py
@@ -2,163 +2,143 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-from .color_palette import color_palette, set_color_palette
+from .color_palette import color_palette
 
 
-class Plot:
+def plot_clusters_catalogue_distribution(data: pd.DataFrame,
+                                         title: str = None,
+                                         xlim: tuple = None,
+                                         ylim: tuple = None,
+                                         hue: str = 'diam'):
+    fig, ax = plt.subplots(figsize=(12, 6))
+    if title is not None:
+        ax.set_title(title)
 
-    default_ext = 'pdf'
+    ax.set_xlabel('Right Ascension (J2000 Degree)')
+    ax.set_ylabel('Declination (J2000 Degree)')
 
-    def __init__(self, save_figs: bool = False, figs_path: str = '.'):
-        self.save_figs = save_figs
-        self.figs_path = figs_path
-        set_color_palette()
+    if xlim is not None:
+        ax.set_xlim(xlim)
+    if ylim is not None:
+        ax.set_ylim(ylim)
 
-    def plot_clusters_catalogue_distribution(self,
-                                             df_catalogue,
-                                             img_name: str,
-                                             img_ext: str = default_ext,
-                                             title: str = None,
-                                             xlim: tuple = None,
-                                             ylim: tuple = None,
-                                             hue: str = 'diam'):
-        fig, ax = plt.subplots(figsize=(12, 6))
-        if title is not None:
-            ax.set_title(title)
+    palette = None
+    if hue is not None:
+        n_colors = len(pd.unique(data[hue]))
+        palette = color_palette(n_colors=n_colors)
 
-        ax.set_xlabel('Right Ascension (J2000 Degree)')
-        ax.set_ylabel('Declination (J2000 Degree)')
+    g = sns.scatterplot(data=data, x="ra", y="dec", hue=hue, size=hue, palette=palette, ax=ax)
 
-        if xlim is not None:
-            ax.set_xlim(xlim)
-        if ylim is not None:
-            ax.set_ylim(ylim)
+    plt.legend().set_title("Diameter (arcmin)")
 
-        g = sns.scatterplot(data=df_catalogue,
-                            x="ra",
-                            y="dec",
-                            hue=hue,
-                            size=hue,
-                            palette=color_palette(n_colors=len(pd.unique(df_catalogue[hue]))),
-                            ax=ax)
-        plt.legend().set_title("Diameter (arcmin)")
+    return fig, ax, g
 
-        if self.save_figs:
-            self._save_figure(fig, name=img_name, img_ext=img_ext)
 
-        return fig, ax, g
+def plot_cluster_proper_motion(data: pd.DataFrame,
+                               title: str = None,
+                               xlim: tuple = None,
+                               ylim: tuple = None,
+                               hue: str = 'cluster_g',
+                               legend: bool = True):
+    fig, ax = plt.subplots(figsize=(6, 6))
+    if title is not None:
+        ax.set_title(title)
 
-    def plot_cluster_proper_motion(self,
-                                   df_cluster,
-                                   img_name: str,
-                                   img_ext: str = default_ext,
-                                   title: str = None,
-                                   xlim: tuple = None,
-                                   ylim: tuple = None,
-                                   hue: str = 'cluster_g',
-                                   legend: bool = True):
-        fig, ax = plt.subplots(figsize=(6, 6))
-        if title is not None:
-            ax.set_title(title)
+    ax.set_xlabel(r'Proper Motion in Right Ascension Direction ($mas \cdot year^{-1}$)')
+    ax.set_ylabel(r'Proper Motion in Declination Direction ($mas \cdot year^{-1}$)')
 
-        ax.set_xlabel(r'Proper Motion in Right Ascension Direction ($mas \cdot year^{-1}$)')
-        ax.set_ylabel(r'Proper Motion in Declination Direction ($mas \cdot year^{-1}$)')
+    if xlim is not None:
+        ax.set_xlim(xlim)
+    if ylim is not None:
+        ax.set_ylim(ylim)
 
-        if xlim is not None:
-            ax.set_xlim(xlim)
-        if ylim is not None:
-            ax.set_ylim(ylim)
+    palette = None
+    if hue is not None:
+        n_colors = len(pd.unique(data[hue]))
+        palette = color_palette(n_colors=n_colors)
 
-        g = sns.scatterplot(data=df_cluster,
-                            x="pmra",
-                            y="pmdec",
-                            hue=hue,
-                            palette=color_palette(n_colors=len(pd.unique(df_cluster[hue]))),
-                            s=12,
-                            ax=ax,
-                            legend=legend)
+    g = sns.scatterplot(data=data, x="pmra", y="pmdec", hue=hue, palette=palette, s=12, ax=ax, legend=legend)
 
-        if self.save_figs:
-            self._save_figure(fig, name=img_name, img_ext=img_ext)
+    return fig, ax, g
 
-        return fig, ax, g
 
-    def plot_cluster_parallax_histogram(self,
-                                        df_cluster,
-                                        img_name: str,
-                                        img_ext: str = default_ext,
-                                        title: str = None,
-                                        bins='auto',
-                                        xlim: tuple = None,
-                                        ylim: tuple = None,
-                                        stat: str = 'count',
-                                        hue: str = 'cluster_g',
-                                        legend: bool = True):
-        fig, ax = plt.subplots(figsize=(6, 6), tight_layout=True)
-        if title is not None:
-            ax.set_title(title)
+def plot_cluster_parallax_histogram(df_cluster,
+                                    title: str = None,
+                                    xlim: tuple = None,
+                                    ylim: tuple = None,
+                                    stat: str = 'count',
+                                    bins='auto',
+                                    hue: str = 'cluster_g',
+                                    legend: bool = True):
+    fig, ax = plt.subplots(figsize=(6, 6), tight_layout=True)
+    if title is not None:
+        ax.set_title(title)
 
-        ax.set_xlabel(r'Parallax ($mas$)')
-        ax.set_ylabel(stat.title())
+    ax.set_xlabel(r'Parallax ($mas$)')
+    ax.set_ylabel(stat.title())
 
-        if xlim is not None:
-            ax.set_xlim(xlim)
-        if ylim is not None:
-            ax.set_ylim(ylim)
+    if xlim is not None:
+        ax.set_xlim(xlim)
+    if ylim is not None:
+        ax.set_ylim(ylim)
 
-        g = sns.histplot(data=df_cluster,
-                         x='parallax',
-                         hue=hue,
-                         palette=color_palette(n_colors=len(pd.unique(df_cluster[hue]))),
-                         legend=legend,
-                         bins=bins,
-                         kde=True,
-                         ax=ax,
-                         stat=stat)
+    palette = None
+    if hue is not None:
+        n_colors = len(pd.unique(df_cluster[hue]))
+        palette = color_palette(n_colors=n_colors)
 
-        if self.save_figs:
-            self._save_figure(fig, name=img_name, img_ext=img_ext)
+    g = sns.histplot(data=df_cluster,
+                     x='parallax',
+                     hue=hue,
+                     palette=palette,
+                     legend=legend,
+                     bins=bins,
+                     kde=True,
+                     ax=ax,
+                     stat=stat)
 
-        return fig, ax, g
+    return fig, ax, g
 
-    def plot_cluster_isochrone_curve(self,
-                                     df_cluster,
-                                     img_name: str,
-                                     img_ext: str = default_ext,
-                                     title: str = None,
-                                     xlim: tuple = None,
-                                     ylim: tuple = None,
-                                     hue: str = 'cluster_g',
-                                     legend: bool = True):
-        fig, ax = plt.subplots(figsize=(6, 6))
-        if title is not None:
-            ax.set_title(title)
 
-        ax.set_xlabel(r'$B_{mag} - R_{mag}$')
-        ax.set_ylabel(r'$G_{mag}$')
+def plot_cluster_isochrone_curve(data: pd.DataFrame,
+                                 title: str = None,
+                                 xlim: tuple = None,
+                                 ylim: tuple = None,
+                                 hue: str = 'cluster_g',
+                                 legend: bool = True):
+    fig, ax = plt.subplots(figsize=(6, 6))
+    if title is not None:
+        ax.set_title(title)
 
-        if xlim is not None:
-            ax.set_xlim(xlim)
-        if ylim is not None:
-            ax.set_ylim(ylim)
+    ax.set_xlabel(r'$B_{mag} - R_{mag}$')
+    ax.set_ylabel(r'$G_{mag}$')
 
-        g = sns.scatterplot(data=df_cluster,
-                            x="bp_rp",
-                            y="phot_g_mean_mag",
-                            hue=hue,
-                            palette=color_palette(n_colors=len(pd.unique(df_cluster[hue]))),
-                            size='parallax',
-                            sizes=(2, 20),
-                            ax=ax,
-                            legend=legend)
+    if xlim is not None:
+        ax.set_xlim(xlim)
+    if ylim is not None:
+        ax.set_ylim(ylim)
 
-        ax.invert_yaxis()
+    palette = None
+    if hue is not None:
+        n_colors = len(pd.unique(data[hue]))
+        palette = color_palette(n_colors=n_colors)
 
-        if self.save_figs:
-            self._save_figure(fig, name=img_name, img_ext=img_ext)
+    g = sns.scatterplot(data=data,
+                        x="bp_rp",
+                        y="phot_g_mean_mag",
+                        hue=hue,
+                        size='parallax',
+                        sizes=(2, 20),
+                        palette=palette,
+                        ax=ax,
+                        legend=legend)
 
-        return fig, ax, g
+    ax.invert_yaxis()
 
-    def _save_figure(self, fig, name: str, img_ext: str):
+    return fig, ax, g
+
+
+def save_figure(fig, name: str, img_ext: str = "pdf", save_dir='./'):
+    if save_dir is not None:
         name = name.replace(' ', '_').lower()
-        fig.savefig(f"{self.figs_path}/{name}.{img_ext}")
+        fig.savefig(f"{save_dir}/{name}.{img_ext}")
